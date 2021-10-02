58677aad5ccbd32389df7f405401133ca03b2fd7
Citlalli1321 <20203tn037@utez.edu.mx>
2021-10-02 02:21:22 -0500

    Costumers

---
+++ src/main/java/mx/edu/utez/rest/Customer/controller/ServiceCustomer.java
@@ -0,0 +1,57 @@
+package mx.edu.utez.rest.Customer.controller;
+
+import mx.edu.utez.rest.Customer.model.Customer;
+import mx.edu.utez.rest.Customer.model.DaoCustomer;
+
+import java.awt.*;
+
+public class ServiceCustomer {
+
+
+    public List<Customer> getCustomer(){
+        return new DaoCustomer().findAll();
+    }
+
+    public Customer getCustomer(int customerNumber){
+        return new DaoCustomer().findByCustomerNumber(customerNumber);
+    }
+
+    @Consumes("application/x-www-form-urlencoded")
+    public Customer save(MultivaluedMap<String, String> formParams){
+        int customerNumber = Integer.parseInt(formParams.get("customerNumber").get(0));
+        if(new DaoCustomer().save(getParams(customerNumber, formParams), true))
+            return new DaoCustomer().findByCustomerNumber(customerNumber);
+        return null;
+    }
+
+    @Consumes("application/x-www-form-urlencoded")
+    public Customer save(int customerNumber, MultivaluedMap<String, String> formParams){
+        if(new DaoCustomer().save(getParams(customerNumber, formParams), false))
+            return new DaoCustomer().findByCustomerNumber(customerNumber);
+        return null;
+    }
+
+    public boolean deleteCustomer(int customerNumber){
+        return new DaoCustomer().delete(customerNumber);
+    }
+
+    private Customer getParams(int customerNumber, MultivaluedMap<String, String> formParams){
+
+        String customerName = formParams.get("customerName").get(0);
+        String contactLastName = formParams.get("contactLastName").get(0);
+        String contactFirstName = formParams.get("contactFirstName").get(0);
+        String phone = formParams.get("phone").get(0);
+        String addressLine1 = formParams.get("addressLine1").get(0);
+        String addressLine2 = formParams.get("addressLine2").get(0);
+        String city = formParams.get("city").get(0);
+        String state = formParams.get("state").get(0);
+        String postalCode =formParams.get("postalCode").get(0);
+        String country = formParams.get("country").get(0);
+        int salesRepEmployeeNumber = Integer.parseInt(formParams.get("salesRepEmployeeNumber").get(0));
+        double creditLimit = Double.parseDouble(formParams.get("creditLimit").get(0));
+
+        Customer customer = new Customer(customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city,state,postalCode,country,salesRepEmployeeNumber,creditLimit);
+        System.out.println(customer);
+        return customer;
+    }
+}
